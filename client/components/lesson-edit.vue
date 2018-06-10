d4052801dc1603812fec548ed1c1dbd616e9e18f
james <james.speirs@gmail.com>
2018-06-10 00:36:02 -0600

    start working on lesson edit component

--- client/components/lesson-edit.vue
+++ client/components/lesson-edit.vue
@@ -1,157 +1,79 @@
 <template>
-  <v-form v-model="valid">
-    <v-container grid-list-md>
-      <v-layout row wrap>
-        <v-flex xs12>
-          <v-text-field v-model="data.title" :rules="titleRules" label="Title" required></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-radio-group v-model="data.discoverability" label="Discoverability">
-            <v-radio label="Private" key="private" value="private"></v-radio>
-            <v-radio label="Sharable Link" key="link" value="link"></v-radio>
-            <v-radio label="Public" key="public" value="public"></v-radio>
-          </v-radio-group>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-radio-group v-model="data.guided" label="Instruction">
-            <v-radio label="Self Taught" key="private" value="self"></v-radio>
-            <v-radio label="Instructor Led" key="link" value="instructor"></v-radio>
-            <v-radio label="Self Taught or Instructor Led" key="public" value="both"></v-radio>
-          </v-radio-group>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row>
-        <v-flex xs12>
-          <v-subheader class="section-label">Material</v-subheader>
-          <div>
-            <v-btn color="secondary">
-              <v-icon>add_circle</v-icon>
-              Add a Lesson
-            </v-btn>
-            <v-btn color="secondary">
-              <v-icon>add_circle</v-icon>
-              Add a Project
-            </v-btn>
-            <v-btn color="secondary">
-              <v-icon>add_circle</v-icon>
-              Add a Quiz
-            </v-btn>
-          </div>
-          <v-list two-line>
-            <draggable model="data.materials" class="draggable-list">
-              <template v-for="(item, index) in data.materials">
-                <v-list-tile :key="item.id">
-                  <v-list-tile-avatar :color="item.color">{{item.type}}</v-list-tile-avatar>
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{item.title}}</v-list-tile-title>
-                    <v-list-tile-sub-title>{{item.description}}</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </template>
-            </draggable>
-          </v-list>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-form>
+  <div class="lesson-edit">
+    <div class="buttons">
+      <v-btn color="secondary">Preview</v-btn>
+      <v-btn color="secondary">Reader</v-btn>
+    </div>
+    <div>Pages...</div>
+    <div class="editor">
+      <textarea :value="input" @input="update"></textarea>
+      <div v-html="compiledMarkdown"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import draggable from 'vuedraggable'
-  import { deepCopy } from '~/assets/js/util'
+  import _ from 'lodash'
+  import lessonEdit from '~/components/lesson-edit'
+  import marked from 'marked'
 
   export default {
 
     components: {
-      draggable
-    },
-
-    props: {
-      courseId: {
-        type: String
-      }
+      'lesson-edit': lessonEdit
     },
 
     data() {
-      const course = this.$store.state.trainer.courses[this.courseId]
-
-      const result = {
-        valid: false,
-
-        data: course ? deepCopy(course) : this.getEmptyCourse(),
-
-        titleRules: [
-          v => !!v || 'Title is required'
-        ],
-
-        tagRules: [
-          v => !!v || 'Tag cannot be empty',
-          v => this.data.tags.values.indexOf(v) === -1 || 'Tag already listed'
-        ]
+      return {
+        input: ''
       }
+    },
 
-      result.data.materials = result.data.materials.map(v => this.getMaterial(v))
-
-      return result
+    computed: {
+      compiledMarkdown() {
+        console.log(marked, this.input)
+        return this.input
+          ? marked(this.input, { sanitize: true })
+          : ''
+      }
     },
 
     methods: {
-      getEmptyCourse() {
-        return {
-          id: '',
-          title: '',
-          discoverability: 'private',
-          guided: 'both',
-          materials: [],
-          tags: []
-        }
-      },
-
-      getMaterial(item) {
-        console.log(item);
-        const store = this.$store.state.trainer[item.store]
-
-        if (store && store[item.id]) {
-          const value = store[item.id]
-          const type = item.store[0].toUpperCase()
-          let color = 'error'
-          switch (type) {
-            case 'L':
-              color = 'success'
-              break
-            case 'P':
-              color = 'accent'
-              break
-            case 'Q':
-              color = 'warning'
-              break
-          }
-          return Object.assign({ type, color }, value)
-        } else {
-          return { title: '** NOT FOUND **', type: '!', description: '', color: 'error' }
-        }
-      }
+      update: _.debounce(function(e) {
+        this.input = e.target.value
+      }, 300)
     }
 
   }
 </script>
 
 <style lang="stylus" scoped>
-  .avatar {
-    cursor: move;
-  }
+  .lesson-edit {
+    display: flex;
+    flex-direction: column;
 
-  .draggable-list > * {
-    border-bottom: 1px solid rgba(0, 0, 0, .12);
+    .editor {
+      flex: 1;
+      position: relative;
+      min-height: 250px;
 
-    &:first-child {
-      border-top: 1px solid rgba(0, 0, 0, .12);
+      textarea {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        width: 50%;
+        border: 1px solid #999;
+      }
+
+      div {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        width: 50%;
+        border: 1px solid #999;
+      }
     }
-  }
-
-  .section-label {
-    padding: 0;
-    font-size: 16px;
   }
 </style>
