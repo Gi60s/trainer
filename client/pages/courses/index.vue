e0e025baf9e60769f2f0b2f288b005ea2c705d83
james <james.speirs@gmail.com>
2018-08-28 13:21:41 -0600

    add tabs

--- client/pages/courses/index.vue
+++ client/pages/courses/index.vue
@@ -1,11 +1,42 @@
 <template>
   <section>
-    <p>
-      <v-btn color="primary" @click="edit('')">
-        <v-icon>add_circle</v-icon>
-        Create a Course
-      </v-btn>
-    </p>
+
+    <v-tabs v-model="activeTab">
+      <v-tab key="myCourses">
+        My Courses
+      </v-tab>
+      <v-tab-item key="myCourses">
+        <v-card flat>
+          <v-card-text>
+            <p>You are not currently enrolled in any courses.</p>
+            <p>
+              <v-btn color="primary" class="action-button" @click="activeTab='1'">
+                <v-icon>search</v-icon>
+                Join a Course
+              </v-btn>
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab key="allCourses">
+        All Courses
+      </v-tab>
+      <v-tab-item key="allCourses">
+        <v-card flat>
+          <v-card-text>
+            <p>
+              <v-btn color="primary" class="action-button" @click="edit('')">
+                <v-icon>add_circle</v-icon>
+                Create a Course
+              </v-btn>
+            </p>
+
+            <p>There are no courses.</p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
   </section>
 </template>
 
@@ -30,6 +61,12 @@
     // overwrite default page search
     pageSearch ({ to, from, previous }) {
       return null;
+    },
+
+    data () {
+      return {
+        activeTab: 0
+      }
     },
 
     methods: {
