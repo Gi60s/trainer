8eb4d05575a5d759e6dcb261d89b1cc7e0d7513c
james <james.speirs@gmail.com>
2018-08-28 16:21:51 -0600

    my courses vs all courses

--- client/pages/courses/index.vue
+++ client/pages/courses/index.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
+    <v-btn v-if="$byu.user" absolute bottom right fab color="accent" @click="edit('')">
+      <v-icon>add</v-icon>
+    </v-btn>
 
-    <v-tabs v-model="activeTab">
+    <v-tabs v-model="activeTab" v-if="$byu.user">
       <v-tab key="myCourses">
         My Courses
       </v-tab>
@@ -25,18 +28,37 @@
       <v-tab-item key="allCourses">
         <v-card flat>
           <v-card-text>
-            <p>
-              <v-btn color="primary" class="action-button" @click="edit('')">
-                <v-icon>add_circle</v-icon>
-                Create a Course
-              </v-btn>
-            </p>
 
-            <p>There are no courses.</p>
+            <p v-if="!courses.length">There are no courses.</p>
+            <v-list v-else two-line>
+              <v-list-tile v-for="(course, index) in courses">
+                <v-list-tile-avatar>
+                  <v-icon v-if="course.joined" large color="success">check_circle</v-icon>
+                  <v-icon v-else large>add_circle</v-icon>
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="course.title"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="course.description"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+
           </v-card-text>
         </v-card>
       </v-tab-item>
     </v-tabs>
+
+    <div v-else>
+      <p v-if="!courses.length">There are no courses.</p>
+      <v-list v-else two-line>
+        <v-list-tile v-for="(course, index) in courses">
+          <v-list-tile-content>
+            <v-list-tile-title v-html="course.title"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="course.description"></v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </div>
   </section>
 </template>
 
@@ -63,9 +85,26 @@
       return null;
     },
 
-    data () {
+    asyncData () {
       return {
-        activeTab: 0
+        activeTab: 0,
+        courses: [
+          {
+            title: 'First Course',
+            description: 'This is the first course. You should take it if you do not have anything to do.',
+            joined: true
+          },
+          {
+            title: 'Second Course',
+            description: 'More to do',
+            joined: false
+          },
+          {
+            title: 'Third Course',
+            description: 'Keep it busy',
+            joined: false
+          }
+        ]
       }
     },
 
